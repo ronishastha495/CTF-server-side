112689d6ac9c71b7aeda1c2995f3593318d7c07e
Zkyarpan <karkiarpan555@gmail.com>
2024-06-23 23:02:10 +0545

    complete RBAC

--- src/middlewares/rbacMiddleware.js
+++ src/middlewares/rbacMiddleware.js
@@ -1,31 +1,34 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../users/userModel");
 const config = require("../config/config");
+
 const checkUserRole = (requiredRole) => {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     try {
       const token = req.headers.authorization?.split(" ")[1];
       if (!token) {
         return res.status(403).send("No token provided.");
       }
+
       jwt.verify(token, config.jwtSecret, async (err, decoded) => {
         console.error("JWT Verification Error:", err);
         if (err) {
           return res.status(403).send("Failed to authenticate token.");
         }
-        console.log("Decoded Token:", decoded); // Log the decoded token
-        const userRole = decoded.role; // Assuming the role is stored in the token
-        console.log("User Role:", userRole);
+
+        console.log("Decoded Token:", decoded);
         const user = await UserModel.findById(decoded.sub);
+        console.log(user)
+
         if (!user || user.role !== requiredRole) {
-          return res
-            .status(403)
-            .send(`Forbidden: You do not have the required role.`);
+          return res.status(403).send(`Forbidden: You do not have the required role.`);
         }
+
         next();
       });
     } catch (error) {
       console.error("Unexpected Error During JWT Verification:", error);
+      return res.status(500).send("Internal Server Error");
     }
   };
 };
