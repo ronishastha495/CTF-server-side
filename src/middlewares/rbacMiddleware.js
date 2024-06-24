1611968db9920cf9ecd965e1b75115dd5125c6bf
Zkyarpan <karkiarpan555@gmail.com>
2024-06-24 09:16:43 +0545

    fix some bugs

--- src/middlewares/rbacMiddleware.js
+++ src/middlewares/rbacMiddleware.js
@@ -6,28 +6,20 @@ const checkUserRole = (requiredRole) => {
   return async (req, res, next) => {
     try {
       const token = req.headers.authorization?.split(" ")[1];
+
       if (!token) {
         return res.status(403).send("No token provided.");
       }
-
-      const IdRequest = req.query.id;
 
       jwt.verify(token, config.jwtSecret, async (err, decoded) => {
         if (err) {
           return res.status(403).send("Failed to authenticate token.");
         }
-        if (decoded.sub !== IdRequest) {
-          return res
-            .status(403)
-            .send("Token does not match the requested user ID.");
-        }
 
         const user = await UserModel.findById(decoded.sub);
 
         if (!user || user.role !== requiredRole) {
-          return res
-            .status(403)
-            .send("Forbidden: You do not have the required role.");
+          return res.status(403).send("You do not have the required role.");
         }
 
         next();
