99b6b7bcc8ac15f5fc9092983dae309d0a7b4f81
np05cp4a220020@iic.edu.np <angilneupane222@gmail.com>
2024-06-24 22:34:03 +0545

    auth in ctf create topic api

--- src/ctf-topic/topicRouter.js
+++ src/ctf-topic/topicRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { createTopic ,getTopic} = require("./topicController");
+const { authenticateToken, isAdmin } = require("../middlewares/authHandle");
 
 const topicRouter = express.Router();
 
-topicRouter.post("/createTopic", createTopic);
-topicRouter.get("/getAllTopic", getTopic);
+topicRouter.post("/createTopic",authenticateToken, isAdmin, createTopic);
+topicRouter.get("/getAllTopic",authenticateToken, isAdmin, getTopic);
 
 module.exports = { topicRouter };
