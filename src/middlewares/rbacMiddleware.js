19ce5876da2de4a6491a7a865676cc7e57d1f3b5
Zkyarpan <karkiarpan555@gmail.com>
2024-06-24 08:16:29 +0545

    update RBAC

--- src/middlewares/rbacMiddleware.js
+++ src/middlewares/rbacMiddleware.js
@@ -10,24 +10,29 @@ const checkUserRole = (requiredRole) => {
         return res.status(403).send("No token provided.");
       }
 
+      const IdRequest = req.query.id;
+
       jwt.verify(token, config.jwtSecret, async (err, decoded) => {
-        console.error("JWT Verification Error:", err);
         if (err) {
           return res.status(403).send("Failed to authenticate token.");
         }
+        if (decoded.sub !== IdRequest) {
+          return res
+            .status(403)
+            .send("Token does not match the requested user ID.");
+        }
 
-        console.log("Decoded Token:", decoded);
         const user = await UserModel.findById(decoded.sub);
-        console.log(user)
 
         if (!user || user.role !== requiredRole) {
-          return res.status(403).send(`Forbidden: You do not have the required role.`);
+          return res
+            .status(403)
+            .send("Forbidden: You do not have the required role.");
         }
 
         next();
       });
     } catch (error) {
-      console.error("Unexpected Error During JWT Verification:", error);
       return res.status(500).send("Internal Server Error");
     }
   };
