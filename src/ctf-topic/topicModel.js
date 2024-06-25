170b03b01cfd690441bcd28a7f267bf8664c0f63
Zkyarpan <karkiarpan555@gmail.com>
2024-06-25 18:32:06 +0545

    Fix some bug

--- src/ctf-topic/topicModel.js
+++ src/ctf-topic/topicModel.js
@@ -1,17 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const topicSchema = new mongoose.Schema({
+const topicSchema = new mongoose.Schema(
+  {
     topic: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
-    }
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Check if the model already exists
-const Topic = mongoose.models.Topic || mongoose.model('Topic', topicSchema);
+const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
 
 module.exports = Topic;
